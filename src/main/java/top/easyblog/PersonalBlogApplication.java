26c64f7ec10fd68d1d3795c4494da6ac8c14852f
LoverITer <2489868503@qq.com>
2019-10-02 12:57:14 +0800

    test commit-20191002-1257

---
+++ src/main/java/top/easyblog/PersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package top.easyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalBlogApplication.class, args);
+    }
+
+}
